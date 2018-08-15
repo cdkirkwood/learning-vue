729e0e654e10ab00c83862bc0c0a0c27bdc96267
Connor Kirkwood <cdkirkwood@gmail.com>
2018-08-14 23:11:27 -0400

    completed components exercise

--- Exercise-Components/src/App.vue
+++ Exercise-Components/src/App.vue
@@ -1,39 +1,31 @@
 <template>
     <div class="container">
+        <Header /> 
+        <hr>
         <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
+            <ServerList />
+            <ServerDetails />
         </div>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
+            <Footer />
         </div>
     </div>
 </template>
 
 <script>
+    import Header from './Header.vue'
+    import ServerList from './ServerList.vue'
+    import ServerDetails from './ServerDetails.vue'
+    import Footer from './Footer.vue'
+    export default {
+        components: {
+            Header,
+            ServerList,
+            ServerDetails,
+            Footer
+        }
+    }
 </script>
 
 <style>
